897d15ef30fb01e372ec396dad19afb5592b8fe6
ProffessorPow <89598352+ProffessorPow@users.noreply.github.com>
2022-02-07 19:54:22 -0700

    Added color changing cube

---
+++ version control/Assets/ExmpleBehaviorScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExmpleBehaviorScript : MonoBehaviour
+{
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer> ().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer> ().material.color = Color.blue;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+}
