2a7fd53a21be5bfef3d9225c81f7b9a9b23accd7
ProffessorPow <89598352+ProffessorPow@users.noreply.github.com>
2022-03-16 21:02:32 -0600

    Challenge 1 code

---
+++ Plane challenge/My project/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset = new Vector3(25f, 1.5f, 9f);
+
+        transform.position = plane.transform.position + offset;
+    }
+}
